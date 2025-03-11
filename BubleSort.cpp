19f5b84c1674d0244b3cb333a94b0830c250bd36
Kevin Adrian <kevin19305.ib@gmail.com>
2025-03-11 11:45:09 +0700

    membuat int main

--- BubleSort.cpp
+++ BubleSort.cpp
@@ -74,3 +74,11 @@ void display() {
 	cout << endl;
 }
 
+int main() {
+	input(); // call the input function to get user input
+	BubbleSortArray(); //call the selection sort function
+	display(); //display the sorted array
+	system("pause");
+
+	return 0;
+}
