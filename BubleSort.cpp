c9547ac1dd490e51a8c39655b8c425723b2b47cb
Kevin Adrian <kevin19305.ib@gmail.com>
2025-03-11 11:31:16 +0700

    Commit 1

--- BubleSort.cpp
+++ BubleSort.cpp
@@ -1,69 +1,82 @@
 #include <iostream>
 using namespace std;
 
-int arr[20];           //deklarasi array a dengan ukuran 20
-int n;                 //deklarasi variabel n untuk menyimpan banyaknya elemen
+int arr[20];  
+int n;        
 
-void input() {         //prosedur untuk input
+void input() {   
 	while (true) {
-		cout << "masukan banyaknya elemen pada array : ";   //output ke layar
-		cin >> n; //input dari pengguna
-		if (n <= 20) //jika n kurang dari atau sama dengan 20
-			break; //keluar dari loop
-		else {     //jika n lebih dari 20
-			cout << "\narray dapat mempunyai maksimal 20 elemen.\n"; //output ke layar
+		cout << "input maximum length: "; 
+		cin >> n;   
+		if (n <= 20)  
+			break;    
+		else {        
+			cout << "n is bigger than 20. \n";   
 		}
 	}
-	cout << endl;                          //output baris kosong
-	cout << "===================" << endl; //output ke layar
-	cout << "masukan elemen array" << endl;//output ke layar
-	cout << "===================" << endl; //output ke layar
+	cout << endl;                           //output baris kosong
+	cout << "===================" << endl;  //output ke layar
+	cout << "enter elemen of array" << endl; //output ke layar
+	cout << "===================" << endl;  //output ke layar
 
 	for (int i = 0; i < n; i++) {
-		cout << "data ke-" << (i + 1) << ": ";
+		cout << "data -" << (i + 1) << "; ";
 		cin >> arr[i];
+        cout << endl;
 	}
 }
-void bubbleshortarray() { // prosedur untuk mengurutkan array dengan metode bubble short
+
+void BubbleShortArray() { // procedur untuk mengurutkan array dengan metode bubble sort
 	int pass = 1; // step 1
+
 	do {
-		for (int j = 0; j <= n - 1 - pass; j++) { //step 2
-			if (arr[j] > arr[j + 1]) {            //step 3
+		//repeat step 3 varying j from 0 to n-1 pass
+		for (int j = 0; j <= n - 1 - pass; j++) 
+		{
+			//if element at index j is great
+			//j+1, swap the two element
+			if (arr[j] > arr[j + 1]) {
 				int temp = arr[j];
+				temp = arr[j];
 				arr[j] = arr[j + 1];
 				arr[j + 1] = temp;
 			}
-		}
-		pass = pass + 1;      //step 4
 
-		cout << "\npass " << pass - 1 << ": "; // output ke layar
-		for (int k = 0; k < n; k++) {        // looping nilai k dimulai dari 0 hingga n-1
-			cout << arr[k] << " ";           //output ke layar
+		}
+		//increment pass by
+		pass = pass + 1; //step 4
+
+		cout << "\nPass " << pass - 1 << -1 << "; ";   //output ke layar
+		for (int k = 0; k < n; k++) {             //looping nilai k dimulai dari 0 hingga n-1
+			cout << arr[k] << " ";                //output ke layar
 
 		}
 		cout << endl;
-	} while (pass <= n - 1);   //step 5
+	} while (pass <= n - 1);
 }
+
 void display() {
 	cout << endl;
-	cout << "=================" << endl;
-	cout << "element array yang telah tersusun" << endl;
-	cout << "=================" << endl;
+	cout << "================================" << endl;
+	cout << "Sorted Array" << endl;
+	cout << "================================" << endl;
 	cout << endl;
-	for (int j = 0; j < n; j++) {
-		cout << arr[j];
-		if (j < n - 1) {
+
+	for (int i = 0; i < n; i++) {
+		cout << "Data" << i+1 << ": " << arr[i] << endl;
+		if (i < n - 1) {
 			cout << " --> ";
 		}
 	}
 	cout << endl;
 	cout << endl;
-	cout << "jumlah pass = " << n - 1 << endl;
+	cout << "Jumlah pass = " << n - 1 << endl;
 	cout << endl;
 }
+
 int main() {
 	input(); // call the input function to get user input
-	bubbleshortarray(); //call the selection sort function
+	BubbleShortArray(); //call the selection sort function
 	display(); //display the sorted array
 	system("pause");
 
